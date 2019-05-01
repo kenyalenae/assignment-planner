1d866a5d5b1afdfc082af2f9bacff76e8978b89f
KenyaM <email@company.com>
2019-05-01 14:48:06 -0500

    working on exporting to excel

--- src/main/java/ExportToExcel.java
+++ src/main/java/ExportToExcel.java
@@ -1,2 +1,18 @@
+/* class to export table to excel
+so that user can keep a list of assignments on computer
+and they can edit the list with different fonts/make other changes
+or they could email the list to themselves to have on other devices
+*/
+
 public class ExportToExcel {
+
+    /*
+    I am basing my code off of code I found on the website
+    http://thinktibits.blogspot.com/2012/12/POI-Create-Excel-File-JDBC-Oracle-Table-Data-Example.html
+    */
+
+
+
+
+
 }
